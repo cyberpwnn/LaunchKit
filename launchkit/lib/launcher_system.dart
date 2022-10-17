cd4b9539363c08608821a54b964e467b1d9982cf
cyberpwn <danielmillst@gmail.com>
2022-10-16 21:37:51 -0400

    f

--- launchkit/lib/launcher_system.dart
+++ launchkit/lib/launcher_system.dart
@@ -1,4 +1,6 @@
+import 'package:colorize/colorize.dart';
 import 'package:launchkit/sugar.dart';
+import 'package:microshaft/microshaft.dart';
 
 const String version = "2.0.0";
 
@@ -9,5 +11,14 @@ class LauncherSystem {
 
   Future<void> execute(List<String> arguments) async {
     info("LaunchKit $version");
+    await MicroshaftClient(storage: FileStorage.load("$path/tokens.dat"))
+        .authenticate((url, code) {
+      print(Colorize("Please Visit ").white().toString() +
+          Colorize(url).lightBlue().toString() +
+          Colorize(" and enter the code ").white().toString() +
+          Colorize(code).bgCyan().underline().toString());
+    }).then((value) {
+      warn("Hello ${value.profileName}, ${value.uuid}!");
+    });
   }
 }
